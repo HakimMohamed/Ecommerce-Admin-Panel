81e530c98c5683a8929d0b4a81222e6adccc2a13
HakimMohamed <iabdelhakimmohamed@gmail.com>
2025-03-21 20:40:02 +0200

    added use client for the entire project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,11 @@
+"use client"; // Forces all child components to be client-only
+
 import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
-import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
 
 export default function RootLayout({
   children,
